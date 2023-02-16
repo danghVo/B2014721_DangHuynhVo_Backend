78cc309c85d897e39d4b98e5438b6fd0278fed7b
Vo <vob2014721@student.ctu.edu.vn>
2023-02-16 08:44:46 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -14,4 +14,14 @@ app.get("/", (req, res) => {
 
 app.use("/api/contacts", contactsRouter)
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+})
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    })
+})
+
 module.exports = app
